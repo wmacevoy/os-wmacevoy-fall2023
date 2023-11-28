d118e8ce175e2a75f725dc659d3da82f2c7eeee3
Warren MacEvoy <wmacevoy@gmail.com>
2023-11-28 12:33:03 -0700

    old notes

--- hello.cpp
+++ hello.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int x = 3456;
 
 void say_hello() {
+    // .. sp infinity - ded0
+
     string message = "hello";
     cout << message << endl;
 }
 
 int main() {
+    // ... sp infinity - df20
     say_hello();
     return 0;
 }
